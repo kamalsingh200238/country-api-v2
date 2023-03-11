ac7b9534da35678ff098ea2429f73c85ac9da5d9
Kamal Singh <kamalsingh200238@gmail.com>
2023-03-11 20:29:04 +0530

    feat: fetch the border countries data on server

--- pages/country/[countryCode].tsx
+++ pages/country/[countryCode].tsx
@@ -24,32 +24,55 @@ export async function getStaticPaths() {
 }
 
 export async function getStaticProps(context) {
-  let response
+  let response;
   try {
-    response = await fetch(`https://restcountries.com/v3.1/alpha/${context.params.countryCode}`)
+    response = await fetch(
+      `https://restcountries.com/v3.1/alpha/${context.params.countryCode}`
+    );
   } catch (error) {
     console.error("There was an error and", error);
   }
 
   if (!response?.ok) {
-    console.error(`There was an error and the HTTP Code is: ${response?.status}`)
+    console.error(
+      `There was an error and the HTTP Code is: ${response?.status}`
+    );
   }
 
-  const data = await response?.json()
+  const data = (await response?.json()) as CountryData;
+
+  let resp;
+
+  if (data[0].borders) {
+    resp = await Promise.all(
+      data[0].borders?.map(async (border) => {
+      const resp = await fetch(
+        `https://restcountries.com/v3.1/alpha/${border}`
+      );
+      return await resp.json();
+      })
+    );
+  }
+  const borderCountries = resp?.map(item => item[0].name.common) ?? null
 
   return {
     props: {
       data: data[0],
+      borderCountries: borderCountries,
     }, // will be passed to the page component as props
   };
 }
 
-export default function Page({ data }: {
-  data: CountryData
+export default function Page({
+  data,
+  borderCountries,
+}: {
+  data: CountryData;
+  borderCountries: string[];
 }) {
   const {
     query: { countryCode },
   } = useRouter();
-  console.log(data);
+  console.log({ data, borderCountries });
   return <div>{data.name.official}</div>;
 }
