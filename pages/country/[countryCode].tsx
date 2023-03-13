3b61827d7e2e68ab4be8227831b6e21d672235ee
Kamal Singh <kamalsingh200238@gmail.com>
2023-03-14 01:52:48 +0530

    feat: html done

--- pages/country/[countryCode].tsx
+++ pages/country/[countryCode].tsx
@@ -1,5 +1,7 @@
+import CardDetail from "@/components/CardDetail";
 import { CountryData } from "@/types";
 import { GetStaticPropsContext, InferGetStaticPropsType } from "next";
+import Image from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/router";
 
@@ -80,20 +82,93 @@ export default function Page({
     query: { countryCode },
   } = useRouter();
 
+  function getCurrencies(currencies: {
+    [key: string]: {
+      name: string;
+      symbol: string;
+    };
+  }): string {
+    // check if object is not undefined
+    // create a variable to store the curreny string
+    if (currencies) {
+      let currencyString: string[] = [];
+
+      if (Object.keys(currencies).length === 0) {
+        return "";
+      }
+
+      // loop over the Object
+      for (const [k] of Object.entries(currencies)) {
+        // push the name of currency in currencyString
+        currencyString.push(currencies[k].name);
+      }
+
+      // join the array into string and the return the string
+      return currencyString.join(", ");
+    }
+
+    console.error("object is undefined");
+    return "";
+  }
+
+  function getLanguages(languages: { [key: string]: string }) {
+    // create a variable to store the languages
+    const languagesString: string[] = [];
+
+    // check if the object is empty
+    if (Object.keys(languages).length === 0) {
+      // if true return empty string
+      return "";
+    }
+
+    //loop over the object
+    for (const [, v] of Object.entries(languages)) {
+      // push the value in the languages
+      languagesString.push(v);
+    }
+
+    // join the array and the return it
+    return languagesString.join(", ");
+  }
+
   console.log({ data, borderCountries });
 
   return (
     <>
-      <div>{data.name.official}</div>
-      <div className="flex flex-col">
-        {borderCountries?.map((borderCountry) => (
-          <Link
-            key={borderCountry.cca3}
-            href={`/country/${borderCountry.cca3}`}
-          >
-            {borderCountry.name}
-          </Link>
-        ))}
+      <div className="grid lg:grid-cols-2">
+        <div className="relative aspect-video w-40">
+          <Image
+            src={data.flags.svg}
+            fill={true}
+            alt={`Falg of ${data.name.common}`}
+          />
+        </div>
+      </div>
+      <div className="">
+        <div>{data.name.official}</div>
+        <CardDetail
+          detail="Population"
+          value={data.population.toLocaleString("en-US")}
+        />
+        <CardDetail detail="Top level domain" value={data?.tld?.[0] ?? ""} />
+        <CardDetail detail="Region" value={data.region} />
+        <CardDetail detail="Subregion" value={data.subregion} />
+        <CardDetail detail="Languages" value={getLanguages(data.languages)} />
+        <CardDetail detail="Capital" value={data.capital.join(", ")} />
+        <CardDetail
+          detail="Currencies"
+          value={getCurrencies(data.currencies)}
+        />
+        <div className="flex gap-5 flex-wrap">
+          {borderCountries?.map((borderCountry) => (
+            <Link
+              key={borderCountry.cca3}
+              href={`/country/${borderCountry.cca3}`}
+            >
+              {borderCountry.name}
+            </Link>
+          ))}
+        </div>
       </div>
     </>
   );
