5e5ee23dd47b29e77fc06d8a18653f406bea7e3e
Kamal Singh <kamalsingh200238@gmail.com>
2023-03-15 01:02:56 +0530

    style: dark background in dark mode

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,7 +4,7 @@ import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <div className="min-h-screen">
+    <div className="min-h-screen dark:bg-primary">
       <Navbar />
       <Component {...pageProps} />
     </div>
