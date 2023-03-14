190364fb5884c1142ff8c77152ae71d68d03161d
Kamal Singh <kamalsingh200238@gmail.com>
2023-03-15 00:32:03 +0530

    fix: removed ; showing at the end of the document

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,7 +6,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <div className="min-h-screen">
       <Navbar />
-      <Component {...pageProps} />;
+      <Component {...pageProps} />
     </div>
   );
 }
